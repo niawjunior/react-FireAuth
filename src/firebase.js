61e077cdccf0f44408f9a51c004659d229a2e8f6
niawjunior <pasupol@dome.cloud>
2017-12-28 16:34:01 +0700

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+const config = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+};
+firebase.initializeApp(config);
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
+export default firebase;
